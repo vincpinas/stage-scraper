521839f6be28d1f1c77b88b22ea3f9e7e5606461
Vincent Pinas <vincentpinas@Vincents-MacBook-Pro.local>
2025-07-21 14:40:47 +0200

    response structure & auto sending refactor

--- src/middleware/response.ts
+++ src/middleware/response.ts
@@ -3,32 +3,36 @@ import HttpResponse from "@/models/response.ts";
 import { NextFunction, Request, Response } from "express";
 
 const setup = (appRef: StageScraper) => {
-    return (req: Request, res: Response, next: NextFunction) => {
-        req.appRef = appRef;
-        req.httpResponse = new HttpResponse();
-        
-        next();
-    };
-}
+	return (req: Request, res: Response, next: NextFunction) => {
+		req.appRef = appRef;
+		req.httpResponse = new HttpResponse();
+
+        const originalSend = res.send;
+
+        // Override the send method
+        res.send = function (data: unknown) {
+            // Your custom logic here - runs before every .send() call
+            req.httpResponse.clean();
+    
+            // Call the original send method
+            return originalSend.call(this, data);
+        };
+
+		next();
+	};
+};
 
 const cleanup = async (req: Request, res: Response) => {
-    const response = req.httpResponse; 
+	const response = req.httpResponse;
 
-    // Only send response if headers haven't been sent yet (i.e., no other middleware has called res.end())
-    if (res.headersSent) {
-        return;
-    }
+	// Only send response if headers haven't been sent yet (i.e., no other middleware has called res.end())
+	if (res.headersSent) {
+		return;
+	}
 
-    response.clean();
+	res.status(200).send(response).end();
+};
 
-    res.status(200).send(response);
-    res.end();
-}
-
-export {
-    setup,
-    cleanup,
-    HttpResponse
-}
+export { setup, cleanup, HttpResponse };
 
 export default HttpResponse;
--- src/models/response.ts
+++ src/models/response.ts
@@ -5,13 +5,13 @@ import type {
 	UserData,
 	ServerError,
 	ServerErrorOptions,
-	DBRow,
+	Error,
 } from "@types";
 
 export default class HttpResponse {
 	user: User | null = null;
-	errors: (DBError | ServerError)[];
-	results: DBRow;
+	errors: Error[];
+	results: unknown[];
 	message: string;
 
 	userSet: boolean = false;
@@ -41,7 +41,7 @@ export default class HttpResponse {
 		});
 
 		// Delete user if user property was not set manually before.
-		if(!this.userSet && this.user === null) {
+		if (!this.userSet && this.user === null) {
 			delete this["user" as keyof this];
 		}
 		delete this["userSet" as keyof this];
@@ -65,43 +65,36 @@ export default class HttpResponse {
 	// ================================
 
 	add(key: string, value: unknown) {
-		const responseParam: any = this[key as keyof this];
+		const responseParam: unknown = this[key as keyof this];
 
 		if (
 			typeof responseParam === "string" &&
 			responseParam !== "" &&
 			typeof value === "string"
 		) {
-			(this as any)[key] = value;
+			(this as Record<string, unknown>)[key] = value;
 
 			return this;
 		}
 
 		if (Array.isArray(responseParam)) {
 			if (Array.isArray(value)) {
-				(this as any)[key] = [...responseParam, ...value];
+				(this as Record<string, unknown>)[key] = [...responseParam, ...value];
 			} else {
-				(this as any)[key] = [...responseParam, value];
+				(this as Record<string, unknown>)[key] = [...responseParam, value];
 			}
 
 			return this;
 		}
 
 		// For other cases, just set the value
-		(this as any)[key] = value;
+		(this as Record<string, unknown>)[key] = value;
 
 		return this;
 	}
 
-	setResults(results: DBRow) {
-		this.results = results;
-
-		return this;
-	}
-
-	addResults(results: any |  any[]) {
-
-		if(Array.isArray(results)) {
+	addResults(results: unknown) {
+		if (Array.isArray(results)) {
 			this.results = [...this.results, ...results];
 		} else {
 			this.results.push(results);
@@ -119,7 +112,7 @@ export default class HttpResponse {
 		return this;
 	}
 
-	addError(error: DBError | ServerError) {
+	addError(error: Error) {
 		this.errors.push(error);
 
 		return this;
